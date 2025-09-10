acead534edc19d956d9110c7e61e556c07fff16d
BERNARDO ANTUNES HECKLER <1137118@ATITUS.LOCAL>
2025-09-10 19:53:10 -0300

    criando novo app para aprender hooks

---
+++ aula-hooks/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Novo Projeto</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    color: '#000',
+    fontSize: 50,
+    fontWeight: 'bold',
+  }
+});
