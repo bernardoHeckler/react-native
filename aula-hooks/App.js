f71197fd1921725f877bc48ae91c50e7e01f58cc
BERNARDO ANTUNES HECKLER <1137118@ATITUS.LOCAL>
2025-09-10 21:25:13 -0300

    tela criada em aula com usestate

--- aula-hooks/App.js
+++ aula-hooks/App.js
@@ -1,25 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Pressable,
+  Button,
+  Image,
+  Alert,
+} from "react-native";
+import { useState } from "react";
+
+import Login from "./Login";
+import Registe from "./Registe";
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+  const [showImage, setShowImage] = useState(true);
+
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+
+  function cadastrar() {}
+
+  function Entrar() {
+    const resultado = contador * contador;
+    Alert.alert("Erro ao fazer o login\n você precisa criar uma tela de login com inputs");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Novo Projeto</Text>
+    <SafeAreaView style={styles.container} showsVerticalScrollIndicator={false}>
+      <Text style={styles.textoColorido}>{contador}</Text>
+
+      <Button
+        title="Clique aqui para somar"
+        onPress={() => setContador(contador + 1)}
+      />
+      <Button
+        title="Clique aqui para diminuir"
+        onPress={() => setContador(contador - 1)}
+      />
+
+      <Button title="Entrar" onPress={() => Entrar()} />
+
+      {showImage && (
+        <Image
+          height={200}
+          width={200}
+          source={{
+            uri: "https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/05/gato-laranja-e1748043537291.jpg?w=1200&h=1200&crop=1",
+          }}
+        />
+      )}
+
+      <Button
+        title={showImage ? "Esconder Imagem" : "Mostrar Imagem"}
+        onPress={() => setShowImage(!showImage)}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   texto: {
-    color: '#000',
-    fontSize: 50,
-    fontWeight: 'bold',
-  }
+    fontSize: 30,
+    color: "orange",
+  },
+  textoColorido: {
+    color: "blue",
+    fontSize: 55,
+  },
 });
